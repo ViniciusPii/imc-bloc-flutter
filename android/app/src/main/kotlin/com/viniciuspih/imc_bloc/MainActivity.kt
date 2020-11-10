f7f74a75b5ad4d6f41b33dac9380a4fefcb78479
ViniciusPii <vinysales@hotmail.com>
2020-11-10 16:51:51 -0300

    feat(estrutura) Inicio do projeto e estrutura

---
+++ android/app/src/main/kotlin/com/viniciuspih/imc_bloc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.viniciuspih.imc_bloc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/my_app.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:imc_bloc/src/pages/home.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Home(),
+    );
+  }
+}
